d23263608fc3162044636a67e25d76e5455a7254
Lexi <you@example.com>
2017-07-26 15:03:59 -0500

    Added giphy

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,52 +1,52 @@
 var friendsobj = [
 	{
-		name:"Robb Stark",
-		photo:"http://images6.fanpop.com/image/photos/34200000/got-game-of-thrones-34230157-500-500.png",
+		name:"Amy",
+		photo:"https://media.giphy.com/media/l2SpPS6byAJzlRpS0/giphy.gif",
 		scores:["1","1","1","1","1","1","1","1","1","1"]
 	},
 	{
-		name:"Jhon Snow",
-		photo:"http://winteriscoming.net/wp-content/uploads/2015/01/Jon-Snow-Hair-GIF-Game-of-Thrones.gif",
+		name:"Louis",
+		photo:"https://media.giphy.com/media/2rc0zZAlq79eM/giphy.gif",
 		scores:["2","2","2","2","2","2","2","2","2","2"]
 	},
 	{
-		name:"Daenerys",
-		photo:"https://s-media-cache-ak0.pinimg.com/564x/8a/18/16/8a1816723ba72d5385f6d89c19d65db2.jpg",
+		name:"Jerry",
+		photo:"https://media.giphy.com/media/OhFuOJn2rbLsA/giphy.gif",
 		scores:["3","3","3","3","3","3","3","3","3","3"]
 	},
 	{
-		name:"Tyrion",
-		photo:"http://www.femalefirst.co.uk/image-library/square/500/g/game-of-thrones-season-6-first-look-picture-tyrion.jpg",
+		name:"Demetri",
+		photo:"https://media.giphy.com/media/cAnc192YRQqqs/giphy.gif",
 		scores:["4","4","4","4","4","4","4","4","4","4"]
 	},
 	{
-		name:"Joffrey",
-		photo:"http://cdn.idigitaltimes.com/sites/idigitaltimes.com/files/styles/embed/public/2014/08/28/2014/01/17/14019.jpg",
+		name:"Fred",
+		photo:"https://media.giphy.com/media/l0K45yrMWuTMlteM0/giphy.gif",
 		scores:["5","5","5","5","5","5","5","5","5","5"]
 	},
 	{
-		name:"Ygritte",
-		photo:"https://shwetasdiaries.files.wordpress.com/2015/10/c__data_users_defapps_appdata_internetexplorer_temp_saved-images_got-game-of-thrones-29950776-500-500.png",
+		name:"Chris",
+		photo:"https://media.giphy.com/media/CiYImHHBivpAs/giphy.gif",
 		scores:["1","2","1","2","1","2","1","2","1","2"]
 	},
 	{
-		name:"KHAL-Drogo",
-		photo:"http://images6.fanpop.com/image/photos/38900000/Khal-Drogo-game-of-thrones-38904151-500-500.gif",
+		name:"Eddie",
+		photo:"https://media.giphy.com/media/fAFg3xESCJyw/giphy.gif",
 		scores:["2","1","2","1","2","1","2","1","2","1"]
 	},
 	{
-		name:"Sansa",
-		photo:"http://66.media.tumblr.com/10abd696328b3efc6243add40f63dd41/tumblr_inline_nmm48kWfqv1shq1o2_500.jpg",
+		name:"Melissa",
+		photo:"https://media.giphy.com/media/xTxHuqGPcs18c/giphy.gif",
 		scores:["3","2","1","3","2","1","3","2","1","3"]
 	},
 	{
-		name:"Arya",
-		photo:"https://media.giphy.com/media/fmd46gcrNQJePMU6xa/giphy.gif",
+		name:"Tina",
+		photo:"https://media.giphy.com/media/3o7TKpG9OOUmY7gRy0/giphy.gif",
 		scores:["2","2","3","1","2","2","1","2","3","2"]
 	},
 	{
-		name:"Margaery",
-		photo:"http://images5.fanpop.com/image/photos/30300000/Game-Of-Thrones-Margaery-Tyrell-natalie-dormer-30399439-500-500.png",
+		name:"Ilana",
+		photo:"https://media.giphy.com/media/26uf7nIJBFZKyQQYE/giphy.gif",
 		scores:["5","1","4","5","2","3","2","5","5","5"]
 	},
 	
